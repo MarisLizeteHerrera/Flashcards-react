ba1722f3139f767f30f73d68c8b218af17925c65
Maris Herrera <maris.lizete.herrera@gmail.com>
2018-12-05 22:22:16 -0700

    add form function

--- src/App.js
+++ src/App.js
@@ -12,12 +12,24 @@ class App extends Component {
     ],
   };
 
+  getId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
 
+  addFLAcard = (flacardData) => {
+    let flacard = { id: this.getId(), ...flacardData, };
+    this.setState({ flacards: [flacard, ...this.state.flacards], });
+  };
 
   render() {
     return (
       <Container style={{ paddingTop: "25px" }}>
       <Header as="h1">Flashcards</Header>
+      < br/>
+      <FLAcardForm add={this.addFLAcard} />
+      < br/>
       <FLAcards flacards={this.state.flacards} />
       </Container>
     );
--- src/FLAcards.js
+++ src/FLAcards.js
@@ -5,11 +5,11 @@ import { Grid } from "semantic-ui-react";
 
 const FLAcards = ({ flacards }) => (
 
-  <Grid columns="four" centered divided>
+  <Grid columns="four" centered>
     <Grid.Row>
-
       {flacards.map( flacard => ( 
         <Grid.Column>
+          < br/>
           <FLAcard key={flacard.id} {...flacard}/>
         </Grid.Column>
       ))
